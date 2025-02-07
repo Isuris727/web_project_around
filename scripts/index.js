1f265b1fc34e6d12836396ee865feec2071529bd
Isuris <ajudithsalasr@gmail.com>
2025-02-06 21:47:28 -0600

    refactor (card) like button

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,17 @@
-let profileInfo = document.querySelector(".profile__info");
-let profilePopup = document.querySelector(".profile__popup");
-let profileName = document.querySelector(".profile__name");
-let profileAboutMe = document.querySelector(".profile__about-me");
-let buttonEditProfile = profileInfo.querySelector(".button_type_edit");
-let buttonClosePopup = profilePopup.querySelector(".button_type_close");
-let formPopup = profilePopup.querySelector(".form");
-let formInputName = profilePopup.querySelector(".form__input_type_name");
-let formInputAboutme = profilePopup.querySelector(".form__input_type_about-me");
+const profileInfo = document.querySelector(".profile__info");
+const profilePopup = document.querySelector(".profile__popup");
+const profileName = document.querySelector(".profile__name");
+const profileAboutMe = document.querySelector(".profile__about-me");
+const buttonEditProfile = profileInfo.querySelector(".button_type_edit");
+const buttonClosePopup = profilePopup.querySelector(".button_type_close");
+const formPopup = profilePopup.querySelector(".form");
+const formInputName = profilePopup.querySelector(".form__input_type_name");
+const formInputAboutme = profilePopup.querySelector(
+  ".form__input_type_about-me"
+);
+const elementsCards = document.querySelector(".elements__cards");
+const cardTemplate = document.querySelector(".elements__cards-template");
+const initialCardsData = [{ name: "", src: "" }];
 
 function handleOpenPopup() {
   profilePopup.classList.add("profile__popup_opened");
@@ -22,6 +27,18 @@ function handleEditProfileInfo(event) {
   profileAboutMe.textContent = formInputAboutme.value;
   closePopup();
 }
+
+function createCard(src, name) {
+  const card = cardTemplate.cloneNode(true).content.querySelector(".card");
+  const cardImg = card.querySelector(".card__img");
+  const cardName = card.querySelector(".card__name");
+  const buttonDeleteCard = card.querySelector(".button_type_delete");
+  const buttonlikeCard = card.querySelector(".button_type_delete");
+}
+
+function likeCard() {}
+
+function deleteCard() {}
 
 buttonEditProfile.addEventListener("click", handleOpenPopup);
 buttonClosePopup.addEventListener("click", function () {
