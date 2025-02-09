2c51d9b2af5f39e5d2ed49845f40e70524172137
Isuris <ajudithsalasr@gmail.com>
2025-02-09 00:53:47 -0600

    feat (elements) add card function

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,28 @@
+// ----Profile section
 const profileInfo = document.querySelector(".profile__info");
 const profilePopup = document.querySelector(".profile__popup");
+
 const profileName = document.querySelector(".profile__name");
 const profileAboutMe = document.querySelector(".profile__about-me");
-const popupForm = document.querySelector(".form");
+const formInputName = profilePopup.querySelector(".form__input_type_name");
+const formInputAboutme = profilePopup.querySelector(".form__input_type_about");
 const buttonEditProfile = profileInfo.querySelector(".button_type_edit");
 const buttonCloseProfilePopup =
   profilePopup.querySelector(".button_type_close");
-const buttonAddCard = document.querySelector(".button_type_add");
-const formInputName = profilePopup.querySelector(".form__input_type_name");
-const formInputAboutme = profilePopup.querySelector(
-  ".form__input_type_about-me"
-);
-const elementsPopup = document.querySelector(".elements__popup");
+const profilePopupForm = document.querySelector(".profile__form");
+// ----- Elements section
 const elementsCards = document.querySelector(".elements__cards");
+const elementsPopup = document.querySelector(".elements__popup");
 const cardTemplate = document.querySelector(".elements__card-template");
+const formInputPlaceName = elementsPopup.querySelector(
+  ".form__input_type_name"
+);
+const buttonCloseAddCardPopup =
+  elementsPopup.querySelector(".button_type_close");
+const formInputLink = elementsPopup.querySelector(".form__input_type_about");
+const buttonAddCard = document.querySelector(".button_type_add");
+const elementsPopupForm = document.querySelector(".elements__form");
+// ----- General
 const initialCardsData = [
   { name: "Lago di Braies", src: "./images/image-lago-di-braies.png" },
   { name: "Lago louise", src: "./images/image_lago-louise.png" },
@@ -26,21 +35,33 @@ const initialCardsData = [
   { name: "Valle de Yosemite", src: "./images/image_yosemite-valley.png" },
 ];
 
-function handleOpenPopup() {
-  const activePopup = popupForm.closest(".popup");
-  activePopup.classList.add("popup_opened");
+function handleOpenPopupProfile() {
+  profilePopup.classList.add("popup_opened");
 }
 
-function closePopup() {
-  const activePopup = popupForm.closest(".popup");
-  activePopup.classList.remove("popup_opened");
+function closePopupProfile() {
+  profilePopup.classList.remove("popup_opened");
 }
 
-function handleEditProfileInfo(event) {
-  event.preventDefault();
+function handleOpenPopupAddCard() {
+  elementsPopup.classList.add("popup_opened");
+}
+
+function closePopupAddCard() {
+  elementsPopup.classList.remove("popup_opened");
+}
+
+function handleEditProfileInfo(evt) {
+  evt.preventDefault();
   profileName.textContent = formInputName.value;
   profileAboutMe.textContent = formInputAboutme.value;
-  closePopup();
+  closePopupProfile();
+}
+
+function handleAddCard(evt) {
+  createCard(formInputPlaceName.value, formInputLink.value);
+  evt.preventDefault();
+  closePopupAddCard();
 }
 
 function createCard(name, src) {
@@ -75,16 +96,18 @@ function createCard(name, src) {
   buttonLikeCard.addEventListener("click", likeCard);
 }
 
-buttonEditProfile.addEventListener("click", handleOpenPopup);
-buttonCloseProfilePopup.addEventListener("click", function () {
-  closePopup();
-});
+// ----- Botones
 
-buttonAddCard.addEventListener("click", function () {
-  createCard();
-});
+buttonEditProfile.addEventListener("click", handleOpenPopupProfile);
+profilePopupForm.addEventListener("submit", handleEditProfileInfo);
 
-popupForm.addEventListener("submit", handleEditProfileInfo);
+buttonAddCard.addEventListener("click", () => handleOpenPopupAddCard());
+elementsPopupForm.addEventListener("submit", handleAddCard);
+
+buttonCloseProfilePopup.addEventListener("click", () => closePopupProfile());
+buttonCloseAddCardPopup.addEventListener("click", () => closePopupAddCard());
+
+// ----- Valores iniciales
 
 createCard(initialCardsData[0].name, initialCardsData[0].src);
 createCard(initialCardsData[1].name, initialCardsData[1].src);
